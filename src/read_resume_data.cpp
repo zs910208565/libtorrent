bf85b18f86b4d9d6b15ee5d713f619df912fcc33
arvidn <arvid@libtorrent.org>
2018-11-06 09:19:10 +0100

    minor simplification

--- src/read_resume_data.cpp
+++ src/read_resume_data.cpp
@@ -86,8 +86,7 @@ namespace {
 		}
 
 		auto info_hash = rd.dict_find_string_value("info-hash");
-		if (sha1_hash::difference_type(info_hash.size())
-			!= sha1_hash::size())
+		if (info_hash.size() != static_cast<std::size_t>(sha1_hash::size()))
 		{
 			ec = errors::missing_info_hash;
 			return ret;
